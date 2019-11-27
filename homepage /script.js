1e37fc0f850d628fcb745a40e2e13dd20401d1e6
unknown <pierre-alexis.maerten@hetic.netgit config --global user.name pierrealexism>
2019-11-27 23:25:28 +0100

    formulaire

--- homepage /script.js
+++
@@ -1,7 +0,0 @@
-var menu = document.getElementById("menu");
-var opener = document.getElementById("open-menu");
-console.log(opener);
-
-opener.addEventListener("click", function() {
-  menu.classList.toggle("is-open");
-});
