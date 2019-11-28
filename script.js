e4f8cf219894517b083f68ef33bcb34b62193199
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2019-11-28 16:27:43 +0100

    changed burgermenu

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var menu = document.getElementById("menu");
 var opener = document.getElementById("open-menu");
-console.log(opener);
 
 opener.addEventListener("click", function() {
   menu.classList.toggle("is-open");
